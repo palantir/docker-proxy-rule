c62ceb75be74e7e15651ebaf106ae8547a719b56
Felix de Souza <fdesouza@palantir.com>
2019-11-14 16:58:32 +0000

    CR comments.

--- docker-proxy-junit-jupiter/src/main/java/com/palantir/docker/proxy/DockerProxyExtension.java
+++ docker-proxy-junit-jupiter/src/main/java/com/palantir/docker/proxy/DockerProxyExtension.java
@@ -25,7 +25,7 @@ public final class DockerProxyExtension extends DockerProxyManager<DockerCompose
     DockerProxyExtension(
             Function<DockerExecutable, DockerContainerInfo> dockerContainerInfoCreator,
             Class<?> classToLogFor) {
-        super(customizer -> customizer.apply(DockerComposeExtension.builder()).build(),
+        super(customizer -> customizer.apply(DockerComposeExtension.builder().retryAttempts(0)).build(),
                 dockerContainerInfoCreator,
                 classToLogFor);
     }
